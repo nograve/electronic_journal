a246af64a560f4603d376ad341857038142363ee
nograve <nograveoff@gmail.com>
2021-04-30 12:15:23 +0300

    electronic journal app added

---
+++ lib/electronic_journal_app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ElectronicJournalApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'electronic_journal_app.dart';
+
+void main() {
+  runApp(ElectronicJournalApp());
+}
