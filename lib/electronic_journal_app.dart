e17c687a14639912c8bc96996070330c3fd2ee26
nograve <nograveoff@gmail.com>
2021-04-30 12:17:45 +0300

    changed cupertino lib to material

--- lib/electronic_journal_app.dart
+++ lib/electronic_journal_app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 class ElectronicJournalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
