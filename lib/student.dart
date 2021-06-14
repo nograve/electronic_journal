75532c37dc30adbb58cea9ef1d136597d24d5ad0
nograve <nograveoff@gmail.com>
2021-06-14 18:50:01 +0300

    field id removed

--- lib/student.dart
+++ lib/student.dart
@@ -3,13 +3,12 @@ import 'package:electronic_journal/group.dart';
 import 'package:electronic_journal/user_account.dart';
 
 class Student extends UserAccount {
-  Student(String email, String password, this._id, this._surname, this._name,
+  Student(String email, String password, this._surname, this._name,
       this._patronymic, this._userName, this._userSurname, this._formOfEducation,
       this._phoneNumber, this._group, this._yearOfEntering, this._dateOfBirth,
       this._providedEmail, this._relativeSurname, this._relativeName, this._relativePatronymic,
       this._relativePhoneNumber) : super(email, password);
 
-  int _id;
   String _surname, _name, _patronymic;
   String _userName, _userSurname;
   FormOfEducation _formOfEducation;
