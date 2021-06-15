b0431a7e31e592240f8e22fce484752286cf31f5
nograve <nograveoff@gmail.com>
2021-06-15 14:43:18 +0300

    implement createuser form

--- lib/electronic_journal_app.dart
+++ lib/electronic_journal_app.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:electronic_journal/auth_methods.dart';
+import 'package:electronic_journal/create_user_page.dart';
 import 'package:electronic_journal/user_type.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -29,8 +30,8 @@ class _ElectronicJournalAppState extends State<ElectronicJournalApp> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      print('TabIndex $_selectedIndex');
     });
+    print('TabIndex $_selectedIndex');
   }
 
   void onLogOut() {
@@ -38,28 +39,45 @@ class _ElectronicJournalAppState extends State<ElectronicJournalApp> {
       if (user == null) {
         setState(() {
           _isSignedIn = false;
+          _userType = null;
         });
+        print('User Role is $_userType');
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // Firebase initialized with FutureBuilder
-    return FutureBuilder(
-      future: _fbInit,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
           return MaterialApp(
             // if Signed In - use Home Page
-            home: _isSignedIn ? Scaffold(
-              appBar: AppBar(
-                title: const Text('Електронний журнал'),
-              ),
-              body: _selectedIndex == 0 ? Column(
-                children: [
-                  // Logout button
-                  ElevatedButton(
+            home: Builder(
+              builder: (context) => _isSignedIn ? Scaffold(
+                appBar: AppBar(
+                  title: const Text('Електронний журнал'),
+                ),
+                body: _selectedIndex == 0 ? Column(
+                  children: [
+                    _userType == UserType.Admin ?
+                    ElevatedButton(
+                      child: Column(
+                        children: [
+                          Icon(Icons.add),
+                          Text('Додати користувача'),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CreateUserPage()),
+                        );
+                      },
+                    )
+                        : Container(
+                      width: 0,
+                      height: 0,
+                    ),
+                    // Logout button
+                    ElevatedButton(
                       onPressed: onLogOut,
                       child: Column(
                         children: [
@@ -67,100 +85,94 @@ class _ElectronicJournalAppState extends State<ElectronicJournalApp> {
                           Text('Вийти'),
                         ],
                       ),
-                  ),
-                ],
-              ) : _selectedIndex == 1 ? Column(
-
-              ) : Column(
-
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                items: const <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Головна',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.account_box_outlined),
-                    label: 'Профіль',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.settings),
-                    label: 'Налаштування',
-                  ),
-                ],
-                currentIndex: _selectedIndex,
-                onTap: _onItemTapped,
-              ),
-            )
-                :
-                // if Not Signed In - use Authentication Page
-            Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // email, implement validator
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.email),
-                      labelText: 'Електронна адреса',
-                      hintText: 'Введіть адрес електронної пошти',
                     ),
-                    controller: emailController,
+                  ],
+                ) : _selectedIndex == 1 ? Column(
 
-                  ),
-                  // password, implement validator
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.lock),
-                      labelText: 'Пароль',
-                      hintText: 'Введіть пароль від вашого аккаунта',
+                ) : Column(
+
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Головна',
                     ),
-                    obscureText: true,
-                    controller: passwordController,
-                  ),
-                  // confirm button
-                  ElevatedButton(
-                    onPressed: () {
-                      signIn(
-                          email: emailController.text.trim(),
-                          password: passwordController.text.trim())
-                          .then((user) {
-                        if (user != null) {
-                          setState(() {
-                            _isSignedIn = true;
-                          });
-                          _users.where('email', isEqualTo: emailController.text)
-                              .snapshots().listen((data) {
-                            String userRole = data.docs.first['role'];
-                            if (userRole == 'student') {
-                              _userType = UserType.Student;
-                            } else if (userRole == 'teacher') {
-                              _userType = UserType.Teacher;
-                            } else if (userRole == 'admin_representative') {
-                              _userType = UserType.AdminRepresentative;
-                            } else if (userRole == 'admin') {
-                              _userType = UserType.Admin;
-                            }
-                            print('user role is $userRole');
-                          });
-                        }
-                      });
-                    },
-                    child: Text('Увійти'),
-                  ),
-                ],
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.account_box_outlined),
+                      label: 'Профіль',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.settings),
+                      label: 'Налаштування',
+                    ),
+                  ],
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTapped,
+                ),
+              )
+                  :
+              // if Not Signed In - use Authentication Page
+              Scaffold(
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // email, implement validator
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.email),
+                        labelText: 'Електронна адреса',
+                        hintText: 'Введіть адрес електронної пошти',
+                      ),
+                      controller: emailController,
+
+                    ),
+                    // password, implement validator
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.lock),
+                        labelText: 'Пароль',
+                        hintText: 'Введіть пароль від вашого аккаунта',
+                      ),
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    // confirm button
+                    ElevatedButton(
+                      onPressed: () {
+                        signIn(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim())
+                            .then((user) {
+                          if (user != null) {
+                            setState(() {
+                              _isSignedIn = true;
+                              _users.where('email', isEqualTo: emailController.text)
+                                  .snapshots().listen((data) {
+                                String userRole = data.docs.first['role'];
+                                if (userRole == 'student') {
+                                  _userType = UserType.Student;
+                                } else if (userRole == 'teacher') {
+                                  _userType = UserType.Teacher;
+                                } else if (userRole == 'admin_representative') {
+                                  _userType = UserType.AdminRepresentative;
+                                } else if (userRole == 'admin') {
+                                  _userType = UserType.Admin;
+                                }
+                                print('User Role is $userRole');
+                              });
+                            });
+                          }
+                        });
+                      },
+                      child: Text('Увійти'),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
-        } else if (snapshot.hasError) {
-          return Text('Сталася помилка...');
-        } else {
-          return CircularProgressIndicator();
-        }
-      },
-    );
   }
 }
 
